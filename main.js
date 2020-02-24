57656340b81578e05de964408f64926a3e4778c4
Patrick <patrick.hurley@codeimmersives.com>
2020-02-24 17:57:27 -0500

    one left

--- main.js
+++ main.js
@@ -30,14 +30,38 @@ console.log(yelledGreetings(['hello', 'there', 'you']));
 
 
 
-function changeToInitials() {
+function changeToInitials(wordsArray) {
+  let initials = [];
+  for (let i = 0; i < wordsArray.length; i++){
+   initials.push(wordsArray[i][0] + wordsArray[i][wordsArray[i].indexOf(' ') + 1]);
+  }
+  return initials;
 }
+console.log(changeToInitials(['Colin Jaffe', 'Patrick Hurley']))
 
-function doubleOdd() {
+function doubleOdd(numberArray) {
+  let odds = [];
+  for (let i = 0; i < numberArray.length; i++){
+    if (numberArray[i] % 2 === 1)
+    {odds.push(numberArray[i] * 2);}
+   else {
+    odds.push(numberArray[i]);
+  }
 }
+return odds;
+}
+console.log(doubleOdd([1, 2, 3, 4, 101, 0, 32.5]));
 
-function upperCaseFirstLetters() {
+
+
+function upperCaseFirstLetters(wordsArray) {
+  let firstCap = [];
+  for (let i = 0; i < wordsArray.length; i++){
+  firstCap.push(wordsArray[i][0].toUpperCase() + wordsArray[i].slice(1));
+  }
+  return firstCap;
 }
+console.log(upperCaseFirstLetters(['collin','patrick',]));
 
 function add1ToLeft() {
 }
