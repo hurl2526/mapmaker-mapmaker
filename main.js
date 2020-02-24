8ff964dbbf3bd4fd33698936d183c1398c04d9f4
Patrick <patrick.hurley@codeimmersives.com>
2020-02-24 16:25:34 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-function doubleAll() {
+function doubleAll(numberArray) {
+  let doubles = [];
+  for (let i = 0;i < numberArray.length; i++){
+    doubles.push(numberArray[i] * 2);
+  }
+return doubles;
 }
+console.log(doubleAll([1,2,3,4,]));
 
-function absoluteValues() {
-}
 
-function yelledGreetings() {
+function absoluteValues(numberArray) {
+  let absolute = [];
+  for (let i = 0;i < numberArray.length; i++){
+    absolute.push(Math.abs(numberArray[i]));
+  }
+return absolute;
 }
+console.log(absoluteValues([1,2,3,4,-7]));
+
+
+
+function yelledGreetings(wordsArray) {
+  let yelled = [];
+  for (let i = 0; i < wordsArray.length; i++){
+   yelled.push(wordsArray[i] + '!');
+  }
+  return yelled;
+}
+console.log(yelledGreetings(['hello', 'there', 'you']));
+
+
 
 function changeToInitials() {
 }
